dac6d38c5789fe2255142ed6389e8cc44bf754f5
Thanh Nguyen <thanhn@microsoft.com>
2020-09-05 02:14:31 +1000

    handle subscribe and notify

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -15,27 +16,53 @@ const (
 	PORT = 8080
 )
 
-func handlerCallbackFbGroup(c echo.Context) error {
-	msg := `Request received at %s:
-- Method : %s
-- Query  : %s
-- Request: %s
-`
+var (
+	fbVerifyToken = ""
+)
+
+func handlerCallbackFbGroupSubscribe(c echo.Context) error {
+	msg := "Request received at %s:\n\t%s"
 	method := c.Request().Method
+	uri := c.Request().RequestURI
+	log.Printf(msg, time.Now(), method+" "+uri)
+
+	hubMode := c.QueryParam("hub.mode")
+	if hubMode != "subscribe" {
+		return c.String(http.StatusBadRequest, "invalid hub.mode")
+	}
+
+	hubVerifyToken := c.QueryParam("hub.verify_token")
+	if hubVerifyToken != fbVerifyToken {
+		return c.String(http.StatusBadRequest, "invalid hub.verify_token")
+	}
+
+	return c.String(http.StatusOK, c.QueryParam("hub.challenge"))
+}
+
+func handlerCallbackFbGroupNotify(c echo.Context) error {
+	msg := `Request received at %s:
+	URI    : %s
+	Request: %s
+`
+
+	method := c.Request().Method
+	uri := c.Request().RequestURI
 	body := ""
 	if strings.ToUpper(method) == "POST" {
 		bodyBytes, _ := ioutil.ReadAll(c.Request().Body)
 		body = string(bodyBytes)
 	}
-	log.Printf(msg, time.Now(), method, c.QueryParams().Encode(), body)
+	log.Printf(msg, time.Now(), method+" "+uri, body)
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
 func main() {
+	fbVerifyToken = os.Getenv("FB_VERIFY_TOKEN")
+
 	e := echo.New()
 
-	e.GET("/callback/fbgroup", handlerCallbackFbGroup)
-	e.POST("/callback/fbgroup", handlerCallbackFbGroup)
+	e.GET("/callback/fbgroup", handlerCallbackFbGroupSubscribe)
+	e.POST("/callback/fbgroup", handlerCallbackFbGroupNotify)
 
 	log.Printf("Application on port %d\n", PORT)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", PORT)))
